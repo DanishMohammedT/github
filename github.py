06bb7cab941bb63bd20928fc752b8277fd44be0d
DanishMohammed <danish10.py@gmail.com>
2026-06-17 23:20:58 +0530

    3rd commit. branch1

--- github.py
+++ github.py
@@ -8,3 +8,13 @@ for i in range(5):
         print("*", end="")
     print()
 
+for i in range(5):
+    for j in range(5-i):
+        print("*", end="")
+    print()
+    
+for i in range(5):
+    for j in range(5-i):
+        print("*", end="")
+    print()
+
